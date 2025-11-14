8cec284422c7b4ec7a04d47cea8e29d1037bf819
Omm Prakash Sahu <opsommprakash@gmail.com>
2025-11-14 16:20:22 +0530

    Uncomment axios instance and update baseURL

--- frontend/src/components/utils/axios.js
+++ frontend/src/components/utils/axios.js
@@ -1,25 +1,7 @@
-// import axios from "axios";
-
-// const instance = axios.create({
-//   baseURL: "http://localhost:5000/api/",
-// });
-
-// instance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default instance;
-
-
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://meetx-backend-a0kt.onrender.com/api/",
-  // baseURL: "http://localhost:5000/api/",
+  baseURL: "http://localhost:5000/api/",
 
 });
 
